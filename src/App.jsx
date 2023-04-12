e1aeffa99d3e73afcaa1e6fae3038c782f27cac3
rastakorps <jose.oakenfold@gmail.com>
2023-04-12 00:17:43 -0600

    01-Se agrega componente card y funcion para obtener los primeros pokemons

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import PokemonWikiImage from './img/pokemon-wiki.png'
+import { Card } from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    const getPokemonList = async () => {
+      const url = "https://pokeapi.co/api/v2/pokemon";
+      //nex url https://pokeapi.co/api/v2/pokemon?offset=20&limit=20
+      const response = await fetch(url);
+      const result = await response.json(); 
+      setPokemons(result.results);
+      console.log(result);
+    }
+
+    getPokemonList();
+  }, []);
 
   return (
-    <div className="bg-red-400">
-      <h1>Pokemon wiki</h1>
+    <div className="h-screen overflow-y-scroll bg-[#3761a8]">
+      <div className="h-16 bg-[#ef534f]">
+        <img src={PokemonWikiImage} alt="pokemon-img" className="m-auto"/>
+      </div>
+
+      <div className="pt-40 grid grid-cols-4 gap-4">
+
+        { pokemons.map( (pokemon, index) => (
+          <Card 
+            key={index}
+          />
+        ))}
+      </div>
     </div>
   )
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Card = () => {
+  return (
+    <div className="w-64 bg-white m-4 p-3 rounded-xl">
+        <p>Nombre: <span>Pikachu</span></p>
+        <p>Nombre: <span>Pikachu</span></p>
+        <p>Nombre: <span>Pikachu</span></p>
+        <p>Nombre: <span>Pikachu</span></p>
+    </div>
+  )
+}
