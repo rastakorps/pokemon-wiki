4793af70289e0b51a7e212587cd3e25deef4da3b
rastakorps <jose.oakenfold@gmail.com>
2023-04-13 00:26:14 -0600

    02-Se listan los pokemons y se agrega estilos al card de pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,19 @@ function App() {
 
   useEffect(() => {
     const getPokemonList = async () => {
-      const url = "https://pokeapi.co/api/v2/pokemon";
+      const url = "https://pokeapi.co/api/v2/pokemon?limit=20";
       //nex url https://pokeapi.co/api/v2/pokemon?offset=20&limit=20
       const response = await fetch(url);
       const result = await response.json(); 
-      setPokemons(result.results);
-      console.log(result);
+      const pokemonsList = result.results;
+      const newPokemons = pokemonsList.map(({name, url}) => {
+        const urlParts = url.split('/');
+        const id = urlParts[urlParts.length -2];
+        const pictureUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
+        return {id, name, pictureUrl};
+      });
+
+      setPokemons(newPokemons);
     }
 
     getPokemonList();
@@ -27,9 +34,10 @@ function App() {
 
       <div className="pt-40 grid grid-cols-4 gap-4">
 
-        { pokemons.map( (pokemon, index) => (
+        { pokemons.map( pokemon => (
           <Card 
-            key={index}
+            key={pokemon.id}
+            pokemon={pokemon}
           />
         ))}
       </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 
-export const Card = () => {
+export const Card = ({pokemon}) => {
+  const {id, name, pictureUrl} = pokemon;
+
   return (
-    <div className="w-64 bg-white m-4 p-3 rounded-xl">
-        <p>Nombre: <span>Pikachu</span></p>
-        <p>Nombre: <span>Pikachu</span></p>
-        <p>Nombre: <span>Pikachu</span></p>
-        <p>Nombre: <span>Pikachu</span></p>
+    <div 
+      className="
+        w-64 
+        justify-self-center 
+        bg-white 
+        mb-4 
+        p-3 
+        rounded-xl
+        cursor-pointer
+        hover:bg-red-500
+        hover:text-white
+        transform hover:scale-105 transition duration-300 ease-in-out"
+    >
+        <div className='flex justify-center items-center bg-[#ffcc01] w-10 h-10 rounded-full'><p>#{id}</p></div>
+        <p className='text-xl font-bold'>{name}</p>
+        <img src={pictureUrl} alt="pokemon img" />
     </div>
   )
 }
